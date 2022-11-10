6ec5bf71711b27ba38015f687fb4859914769678
mrchosen <chosenvincent@gmail.com>
2022-11-10 13:27:00 -0800

    Update Navbar.js

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -2,8 +2,23 @@ import React from "react";
 
 const Navbar = ()=> {
     return (
+<<<<<<< Updated upstream
         <nav>
             
+=======
+        <nav className="navbar">
+            <div className="logo-container">
+                <img />
+            </div>
+            <ul className="nav-links">
+                <li>Home</li>
+                <li>About</li>
+                <li>Blog</li>
+                <li>Programs</li>
+            </ul>
+
+            <button>Contact Us</button>
+>>>>>>> Stashed changes
         </nav>
     )
 }
