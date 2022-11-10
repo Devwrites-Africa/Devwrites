2092f288b1fe8df24dc84ad5306e40ebe915ea72
mrchosen <chosenvincent@gmail.com>
2022-11-10 13:05:05 -0800

    Basic Setups

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/Components/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HomePage = ()=> {
+    return (
+        <div className="home-page">
+            <section className="landing-page">
+
+            </section>
+
+            <section className="features-section">
+
+            </section>
+
+            <section className="contributors-section">
+
+            </section>
+        </div>
+    )
+}
