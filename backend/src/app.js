542bf3df003f32e5d422790e0d319c1211a9ebc5
Treasure <obisiket@yahoo.com>
2022-11-10 23:18:36 +0100

    chore: removed dotenv from app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const app = express();
 
 //middlwares
 app.use(express.json());
-app.use(dotenv.config())
 
 module.exports = app;
