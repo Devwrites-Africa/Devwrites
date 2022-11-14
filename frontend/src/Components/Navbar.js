3ede12ffb8565d5979cb1b1a506c1a8a661a02d4
devwritesafrica <117316416+devwritesafrica@users.noreply.github.com>
2022-11-14 13:17:05 +0100

    Update Navbar.js

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { AppBar, Tab, Tabs, Toolbar, Button } from "@mui/material";
+import {useTheme} from "@mui/material";
+import {useMediaQuery} from "@mui/material";
 
 const Navbar = ()=> {
     const theme = useTheme();
@@ -22,44 +24,29 @@ const Navbar = ()=> {
     }
     
     return (
-        <div >
-            <AppBar
-                sx={{background: '#F5F5F5'}}
-                elevation={0}
-                position='relative'
-            >
-                <Toolbar>
-                    <img src="./Images/Logo.png" alt="logo" className='logo' />
-                        { mobileView ? (
-                            <DrawerComponent />
-                        ) : (
-                            <>
-                                <div className='navLink-container'>
-                                    <Link to='' className="active-link">Home</Link>
-                                    <Link to=''>About</Link>
-                                    <Link to=''>Blog</Link>
-                                    <Link to=''>Programs</Link>
-                                </div>
-
-                                <Link to='' className="contact-btn">
-                                    <Button
-                                        sx={styles.contactBtn} 
-                                        variant="contained"
-                                    >
-                                        Contact Us
-                                    </Button>
-                                </Link>
-                            </>
-                            
-                        )}
-                        
-                    
-                    
-                    
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
-}
-
-export default Navbar
+        <AppBar sx={{ background: "#F5F5F5" }} elevation={0}>
+           <Toolbar>
+              <img src="./Images/Logo.png" alt="logo" className="logo" />
+  
+              <div className="navLink-container">
+                 <Link to="" className="active-link">
+                    Home
+                 </Link>
+                 <Link to="/about">About</Link>
+                 <Link to="">Blog</Link>
+                 <Link to="">Programs</Link>
+              </div>
+  
+              <Button
+                 sx={styles.contactBtn}
+                 variant="contained"
+                 className="contact-btn"
+              >
+                 Contact Us
+              </Button>
+           </Toolbar>
+        </AppBar>
+     );
+  };
+  
+  export default Navbar;
