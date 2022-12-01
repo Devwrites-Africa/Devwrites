57a4a9dffcfe0c4b6a848ff49f97e496b26db462
Jacob Kyalo <jacobkyalo43@gmail.com>
2022-12-01 14:52:45 +0300

    added contact us button to Navbar component

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,61 +1,65 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import DrawerComponent from "./DrawerComponent";
-import { 
-    AppBar, 
-    Tab, 
-    Tabs, 
-    Toolbar, 
+import {
+    AppBar,
+    Tab,
+    Tabs,
+    Toolbar,
     Button,
     useTheme,
-    useMediaQuery
+    useMediaQuery,
 } from "@mui/material";
 
-const Navbar = ()=> {
+const Navbar = () => {
     const theme = useTheme();
-    const mobileView = useMediaQuery(theme.breakpoints.down('md'))
+    const mobileView = useMediaQuery(theme.breakpoints.down("md"));
 
-     const styles = {
+    const styles = {
         tab: {
-           color: 'var(--purple)',
-           fontWeight: '700',
-           fontSize: '1.2rem',
-           textDecoration: 'capitalize'
+            color: "var(--purple)",
+            fontWeight: "700",
+            fontSize: "1.2rem",
+            textDecoration: "capitalize",
         },
-    }
-    
+    };
+
     return (
-        <div >
+        <div>
             <AppBar
-                sx={{background: '#F5F5F5'}}
+                sx={{ background: "#F5F5F5" }}
                 elevation={0}
-                position='relative'
+                position="relative"
             >
                 <Toolbar>
-                    <img src="./Images/Logo.png" alt="logo" className='logo' />
-                        { mobileView ? (
-                            <DrawerComponent />
-                        ) : (
-                            // <Tabs >
-                            //     <Tab label='Home' sx={styles.tab}/>
-                            //     <Tab label='About' sx={styles.tab}/>
-                            //     <Tab label='Contributors' sx={styles.tab}/>
-                            //     <Tab label='Blog' sx={styles.tab}/>
-                            //     <Tab label='Programs' sx={styles.tab}/>
-                            // </Tabs>
-                            <div className='navLink-container'>
-                                <Link to='/' className="active-link">Home</Link>
-                                <Link to='/about'>About</Link>
-                                <Link to=''>Contributors</Link>
-                                <Link to=''>Blog</Link>
-                                <Link to=''>Programs</Link>
-                            </div>
-                        )}
-
+                    <img src="./Images/Logo.png" alt="logo" className="logo" />
+                    {mobileView ? (
+                        <DrawerComponent />
+                    ) : (
+                        // <Tabs >
+                        //     <Tab label='Home' sx={styles.tab}/>
+                        //     <Tab label='About' sx={styles.tab}/>
+                        //     <Tab label='Contributors' sx={styles.tab}/>
+                        //     <Tab label='Blog' sx={styles.tab}/>
+                        //     <Tab label='Programs' sx={styles.tab}/>
+                        // </Tabs>
+                        <div className="navLink-container">
+                            <Link to="/" className="active-link">
+                                Home
+                            </Link>
+                            <Link to="/about">About</Link>
+                            <Link to="">Contributors</Link>
+                            <Link to="">Blog</Link>
+                            <Link to="">Programs</Link>
+                            <Link to="" className="landing-page-btn contact-us">
+                                Contact Us
+                            </Link>
+                        </div>
+                    )}
                 </Toolbar>
             </AppBar>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
