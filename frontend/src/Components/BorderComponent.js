1d24ca0aedad686d5a430e8c7f1feba57f7545d7
Jacob Kyalo <jacobkyalo43@gmail.com>
2022-12-07 21:59:16 +0300

    updated border component

--- frontend/src/Components/BorderComponent.js
+++ frontend/src/Components/BorderComponent.js
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const BorderComponent = () => {
-  return (
-    <div style={styles}></div>
-  )
-}
+    return <div style={styles}></div>;
+};
 
-const styles ={
-    paddingTop:'10px',
-    borderBottom:'4px solid #2DB68F',
-    width:'60%',
-
-}
-export default BorderComponent
+const styles = {
+    marginTop: "10px",
+    backgroundColor: " #2DB68F",
+    width: "20%",
+    height: "6px",
+};
+export default BorderComponent;
